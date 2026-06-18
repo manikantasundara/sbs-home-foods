21ecaeae6baba2b23611a9592fc4f2971df0280e
manikantasundara <116244492+manikantasundara@users.noreply.github.com>
2026-06-18 10:46:30 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,421 @@
+const products = {
+  almonds: [
+    {
+      name: "Plain 5 Pcs",
+      image: "https://palleturiputharekulu.com/wp-content/uploads/2024/11/wsp4.jpg",
+      price: "70/-",
+      special: "Premium Quality",
+      NoOfPieces: "5"
+    },
+    {
+      name: "Plain 10 Pcs",
+      image: "https://palleturiputharekulu.com/wp-content/uploads/2024/11/wsp4.jpg",
+      price: "120/-",
+      special: "Premuim Quality",
+      NoOfPieces: "10"
+    },
+    {
+      name: "Dry fruit 5 Pcs",
+      image: "https://palleturipachallu.com/cdn/shop/files/Artboard1_c1fbd604-fed1-45ed-bae7-c1e7e14e8d06.png?v=1754286253&width=990",
+      price: "90/-",
+      NoOfPieces: "5 small"
+      
+      
+      
+    },
+    {
+      name: "Dry fruit 10 Pcs",
+      image: "https://palleturipachallu.com/cdn/shop/files/Artboard1_c1fbd604-fed1-45ed-bae7-c1e7e14e8d06.png?v=1754286253&width=990",
+      price: "140/-",
+      NoOfPieces: "10 big"
+      
+      
+      
+    },
+    {
+      name: "SP Dry fruit 12 Pcs",
+      image: "https://4clowf19snfiudbw.public.blob.vercel-storage.com/bellam-dry-fruit-pootharekulu.webp",
+      price: "200/-",
+      NoOfPieces: "12 big"
+      
+      
+      
+    },
+    {
+      name: "SP Dry fruit 16 Pcs",
+      image: "https://cdn.shopify.com/s/files/1/0651/8895/4328/files/Atreyapuram-Bellam-Jaggery-Kaju-Badam-Pista-Putharekulu-Pootharekulu-1.jpg?v=1669554310",
+      price: "300/-",
+      NoOfPieces: "16 small"
+      
+      
+      
+    },
+    {
+      name: "SP Dry fruit 4 Pcs",
+      image: "https://cdn.shopify.com/s/files/1/0651/8895/4328/files/Atreyapuram-Bellam-Jaggery-Kaju-Badam-Pista-Putharekulu-Pootharekulu-1.jpg?v=1669554310",
+      price: "150/-",
+      NoOfPieces: "4 big"
+      
+      
+      
+    },
+    {
+      name: "SP Dry fruit 4 Pcs",
+      image: "https://cdn.shopify.com/s/files/1/0651/8895/4328/files/Atreyapuram-Bellam-Jaggery-Kaju-Badam-Pista-Putharekulu-Pootharekulu-1.jpg?v=1669554310",
+      price: "110/-",
+      NoOfPieces: "4 small"
+      
+      
+      
+    },
+
+    {
+      name: "Chocolate",
+      image: "https://sweetduet.in/wp-content/uploads/2025/04/chocolate-pootharekulu-fusion-sweet-sweetduet-premium-sweets-hyderabad.jpg.jpg",
+      price: "130/-",
+      NoOfPieces: "5 medium"
+    },
+    {
+      name: "Horlicks",
+      image: "https://palleturiputharekulu.com/wp-content/uploads/2024/11/wsd5.jpg",
+      price: "150/-",
+      NoOfPieces: "5 medium"
+    },
+    {
+      name: "Boost",
+      image: "https://savithrammas.com/site/image/cache/catalog/products/Putharekulu%20Bellam%20Dry%20Fruit/Bellam%20Putharekulu%20(%20Dry%20Fruit)-800x800.jpg",
+      price: "150/-",
+      NoOfPieces: "5 medium"
+    },
+    {
+      name: "SP DryFruit Box",
+      image: "https://dadus.co.in/cdn/shop/files/DFPutharekulu.png?v=1771687216&width=1445",
+      price: "300/-",
+      NoOfPieces: "12 Pcs"
+    },
+    {
+      name: "DryFruit Normal Box",
+      image: "https://m.media-amazon.com/images/I/81H1M7jNLVL._SL1500_.jpg",
+      price: "250/-",
+      NoOfPieces: "12 Pcs"
+    },
+  ],
+
+  cashews: [
+    {
+      name: "Cashew",
+      image: "https://png.pngtree.com/thumb_back/fw800/background/20240730/pngtree-beautiful-bent-cashew-nuts-fresh-raw-cashew-image_15936556.jpg",
+      price: "1000/-",
+      weight: "1 kg"
+    },
+    {
+      name: "Almond",
+      image: "https://img.freepik.com/premium-photo/almond-nut-hd-8k-background-wallpaper-stock-photographic-image_915071-20283.jpg",
+      price: "750/-",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Salt Pista",
+      image: "https://tse3.mm.bing.net/th/id/OIP.OZArOcFqhgdlWGqCcLrpbQHaEJ?pid=Api&P=0&h=180",
+      price: "1300/-",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Salt Pista",
+      image: "https://5.imimg.com/data5/ECOM/Default/2023/11/358517121/EA/BF/KE/11366453/1699065448444-sku-0130-0-500x500.jpeg",
+      price: "400/-",
+      special: "",
+      weight: "250 g"
+    },
+    {
+      name: "Walnut",
+      image: "https://www.thespruceeats.com/thmb/eBSjJYZioV4G1GEjyHm7jZuilO8=/5760x3840/filters:no_upscale():max_bytes(150000):strip_icc()/walnuts-in-a-bowl--626549496-3201183764cb4d94859f9aa5161e64b7.jpg",
+      price: "400/-",
+      special: "",
+      weight: "250 g"
+    },
+    {
+      name: "Anjeer",
+      image: "https://images.healthshots.com/healthshots/en/uploads/2021/10/10093111/ANJEER-1.jpg",
+      price: "400/-",
+      special: "",
+      weight: "250 g"
+    },
+    {
+      name: "Dates",
+      image: "https://idsb.tmgrup.com.tr/ly/uploads/images/2020/05/19/36607.jpg",
+      price: "250/-",
+      special: "",
+      weight: "400 g"
+    },
+    {
+      name: "Black Kismis",
+      image: "https://5.imimg.com/data5/ECOM/Default/2022/6/ZL/OB/IV/13510045/seedless-black-raisina-1000x1000.jpg",
+      price: "300/-",
+      special: "",
+      weight: "250 g"
+    },
+    {
+      name: "Pumpkin seeds",
+      image: "https://static1.backyardbossimages.com/wordpress/wp-content/uploads/2024/10/gathered-pumpkin-seeds-in-bowls.jpg",
+      price: "220/-",
+      special: "",
+      weight: "250 g"
+    },
+    {
+      name: "Chia seeds",
+      image: "https://healthjade.com/wp-content/uploads/2017/08/chia-seeds.jpg",
+      price: "220/-",
+      special: "",
+      weight: "250 g"
+    },
+    {
+      name: "Sunflower seeds",
+      image: "https://www.eatthis.com/wp-content/uploads/sites/4/2022/07/Sunflower-seeds.jpg?quality=82&strip=1",
+      price: "220/-",
+      special: "",
+      weight: "250 g"
+    },
+  ],
+
+  pistachios: [
+    {
+      name: "Chicken ",
+      image: "https://i.pinimg.com/originals/25/59/53/2559531d88ebeedaa86f632a2dd4b858.jpg",
+      price: "",
+      special: "Salted",
+      weight: "500g"
+    },
+
+    {
+      name: "Prawn",
+      image: "https://xantilicious.com/wp-content/uploads/2016/02/IMG_0982123-1.jpg",
+      price: "",
+      special: "Salted",
+      weight: "500g"
+    },
+
+    {
+      name: "Fish",
+      image: "https://www.yummytummyaarthi.com/wp-content/uploads/2024/02/fish-pickle-1-768x538.jpeg",
+      price: "",
+      special: "Salted",
+      weight: "500g"
+    },
+
+    {
+      name: "Chicken boneless",
+      image: "https://jeerapeppers.com/wp-content/uploads/2023/07/non-veg-pickles-BONELESS-CHICKEN-PICKLE-ANDHRA-STYLE_2_11zon.jpg",
+      price: "",
+      special: "Salted",
+      weight: "500g"
+    },
+
+    {
+      name: "Mutton",
+      image: "https://tastysouthindianfoods.com.au/wp-content/uploads/2024/07/Mutton-pickle.jpeg",
+      price: "",
+      special: "Salted",
+      weight: "500g"
+    },
+
+    
+
+  ],
+
+  raisins: [
+    {
+      name: "Gulab jamun",
+      image: "https://recipes.net/wp-content/uploads/2023/05/gulab-jamun-recipe_9fb159dc2674f395436a64666227c988-768x768.jpeg",
+      price: "350",
+      special: "",
+      weight: "1 kg"
+    },
+
+    {
+      name: "Kaju katli",
+      image: "https://thumbs.dreamstime.com/b/kaju-katli-delicious-fresh-healthy-famous-pakistani-mithai-34186137.jpg",
+      price: "1000/-",
+      special: "",
+      weight: "1 kg"
+    },
+
+    {
+      name: "Sunnunda Sugar",
+      image: "https://vaya.in/recipes/wp-content/uploads/2018/03/Sunnundalu.jpg",
+      price: "600",
+      special: "",
+      weight: "1 k"
+    },
+    {
+      name: "Sunnunda Bellam",
+      image: "https://5.imimg.com/data5/QB/QJ/IW/ANDROID-104366177/product-jpeg-1000x1000.jpg",
+      price: "600",
+      special: "",
+      weight: "1 k"
+    },
+    {
+      name: "Laddu",
+      image: "https://img.freepik.com/premium-photo/traditional-indian-laddu-sweets-stacked-silver-tray-with-vibrant-background_1046581-1992.jpg?w=1480",
+      price: "250",
+      special: "",
+      weight: "1 kg"
+    },
+
+    {
+      name: "Dry fruit laddu",
+      image: "https://img.freepik.com/premium-photo/indian-sweet-healthy-dry-fruit-laddu-laddoo_466689-97123.jpg?w=2000",
+      price: "600",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Nethi laddu",
+      image: "https://thumbs.dreamstime.com/b/traditional-methi-powder-ladoo-fenugreek-seeds-having-nutritional-health-benefits-high-fibre-content-usually-consumed-264606999.jpg",
+      price: "400",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Ragi laddu",
+      image: "https://img.freepik.com/premium-photo/nachni-laddu-ragi-laddoo-sweet-balls-made-using-finger-millet-jaggery-ghee_466689-90969.jpg?w=2000",
+      price: "500",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Millets laddu",
+      image: "https://img.freepik.com/premium-photo/bajra-atta-ladoo-kuler-laddoo-millet-flour-laddu-popular-winter-sweet-snack-food-from-india_466689-87360.jpg?w=1380",
+      price: "500",
+      special: "",
+      weight: "1 kg"
+    },
+
+    {
+      name: "Milk burfi",
+      image: "https://static.vecteezy.com/system/resources/previews/016/283/295/large_2x/milk-powder-barfi-also-known-as-mava-burfi-white-khoya-burfi-or-barfee-indian-sweet-food-free-photo.jpg",
+      price: "800/-",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Soan papdi",
+      image: "https://image.freepik.com/free-photo/traditional-indian-candy-soan-papdi-white-plate-with-almond-pistache-gray-concrete_71985-3299.jpg",
+      price: "400",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Nethi Soan papdi",
+      image: "https://img.onmanorama.com/content/dam/mm/en/archive/food/features/images/2025/10/18/soan-papdi-3.jpg",
+      price: "600",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Kaja",
+      image: "https://i.ytimg.com/vi/C2toAjhdSSw/maxresdefault.jpg",
+      price: "400",
+      special: "",
+      weight: "1 kg"
+    },
+    {
+      name: "Chitti kaja",
+      image: "https://i.ytimg.com/vi/ZwA7xHHPttY/maxresdefault.jpg",
+      price: "350",
+      special: "Medium/Small",
+      weight: "1 kg"
+    },
+    {
+      name: "Badhusha",
+      image: "https://i.ytimg.com/vi/9vlqm5WpCyE/maxresdefault.jpg",
+      price: "300/-",
+      special: "",
+      weight: "1 kg"
+    },
+    
+  ],
+
+  walnuts: [
+    {
+      name: "Chekka papads",
+      image: "https://ayurvedam365.com/wp-content/uploads/2022/12/chekka-appadalu.jpg",
+      price: "",
+      special: "",
+      weight: "500g"
+    },
+
+    {
+      name: "Bundhi",
+      image: "https://www.awesomecuisine.com/wp-content/uploads/2011/10/Kara-Boondi.jpg",
+      price: "",
+      special: "",
+      weight: "500g"
+    },
+
+    {
+      name: "Potato chips",
+      image: "https://diyjoy.com/wp-content/uploads/2023/05/homemade-crispy-potato-chips-recipe.jpg",
+      price: "",
+      special: "",
+      weight: "500g"
+    },
+
+    {
+      name: "Chekodi",
+      image: "https://i.pinimg.com/736x/15/6e/0e/156e0e6fd5a2f35c383a63a175c2bc14.jpg",
+      price: "",
+      special: "",
+      weight: "500g"
+    },
+
+    {
+      name: "Fried kaju",
+      image: "https://1.bp.blogspot.com/-k8HRhGFNfkc/Xn6sicOGK6I/AAAAAAAAYbw/JlCGUFoJG6IySBTa5x_hbvfP6zdP_YhhQCLcBGAsYHQ/s1600/masala%2Bcashewnuts%2B12.JPG",
+      price: "",
+      special: "",
+      weight: "500g"
+    },
+
+    
+
+  ]
+};
+
+function showCategory(category) {
+  const gallery = document.getElementById("gallery");
+
+  gallery.innerHTML = "";
+
+  if (!products[category]) {
+    gallery.innerHTML = "<h2>No products found</h2>";
+    return;
+  }
+
+  products[category].forEach(product => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <img src="${product.image}" alt="${product.name}">
+      <h3>${product.name}</h3>
+    `;
+
+    card.addEventListener("click", () => {
+      openDetails(product);
+    });
+
+    gallery.appendChild(card);
+  });
+}
+
+function openDetails(product) {
+  localStorage.setItem(
+    "selectedProduct",
+    JSON.stringify(product)
+  );
+
+  window.location.href = "details.html";
+}
